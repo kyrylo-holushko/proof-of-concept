4eff3edf2a22c07470d48509228c160e4600806e
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-10-31 15:36:10 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import 'materialize-css/dist/css/materialize.min.css'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+    useEffect(()=>{
+        import('materialize-css/dist/js/materialize');
+    },[]);
+
+    return <Component {...pageProps} />
 }
 
 export default MyApp
