6a503c2118dba78cb6d8e68daf8155e90dd9c79b
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-11-05 19:30:58 -0500

    require materialize for navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,22 @@
 import NavigationBar from "./NavigationBar"
 import FooterBar from "./FooterBar"
+import { useEffect, useState } from 'react'
+//import dynamic from "next/dynamic";
+//import M from 'materialize-css'
+import { NavItem, Navbar } from "react-materialize"
 
 export default function Layout(props){
+
+    useEffect(()=>{
+        /* console.log("FIRST");
+        if(typeof window !=='undefined'){
+            console.log("Window loaded!");
+            const M = dynamic(() => import('materialize-css'), {
+                ssr: false,
+            });
+            console.log(M);
+        } */
+    },[]);
 
     return(
         <>
--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,10 +1,48 @@
+//import { useEffect, useState } from "react"
+import { Icon, Navbar, NavItem } from "react-materialize"
+
+if (typeof window !== 'undefined') {
+    require('materialize-css');
+}
 
 
 export default function NavigationBar() {
 
+   //const [M, setM] = useState(()=>()=>null);
+
+    /* const mObj = dynamic(() => import('materialize-css').then((res)=>console.log(res)), {
+        ssr: false,
+    }); */
+
+    /* async function materialize() {
+        const M = await import("materialize-css");
+        M.AutoInit();
+    } */
+        /* import('materialize-css').then(
+            (
+              { default: comp } // get the import from the returned object
+            ) => setM(() => comp) // use updater function to set in state
+        ); */  
+            //const M = require('materialize-css');
+            //M.AutoInit();
+            //const { Icon } = require('react-materialize');
+       
+        
+        
+        //import("materialize-css");
+        //setM(mObj.M);
+        //M.AutoInit();
+        //setM(M);
+        //M.AutoInit();
+  
+    //
     return (
         <>
-        
+            <Navbar menuIcon={<Icon>menu</Icon>}>
+                <NavItem>First</NavItem>
+                <NavItem>Second</NavItem>
+                <NavItem>Third</NavItem>
+            </Navbar>
         </>
     )
 }
