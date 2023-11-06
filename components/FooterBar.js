f62890c2ad369a3f3d2737da4e9462c7f56b1d42
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-11-05 19:56:44 -0500

    custom footer

--- components/FooterBar.js
+++ components/FooterBar.js
@@ -1,10 +1,10 @@
-
+import { Footer } from "react-materialize"
 
 export default function FooterBar(){
 
     return (
         <>
-            
+            <footer className="page-footer custom-footer center-text">© 2014 Copyright Text</footer>
         </>
     )
 }
