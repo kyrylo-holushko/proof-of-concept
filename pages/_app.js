97db8bcfe77034a7ee2461a3e76dec1c0deef911
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-11-05 19:42:46 -0500

    added link to material icons

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,8 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <title>Golden Abalone Bistro</title>
                 <meta name="Golden Abalone Bistro" content="demo next app" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+                <link rel="stylesheet" href="path/to/materialize.css"></link>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Layout>
