3dbce916bb0ad226881d100de2d90397128644f6
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-11-10 11:17:29 -0500

    menu colomns set up

--- pages/menu.js
+++ pages/menu.js
@@ -1,4 +1,4 @@
-import { Button } from "react-materialize"
+import { Container, Row, Col, Card, CardTitle, Icon } from "react-materialize"
 import useSWR from "swr"
 import { useState } from "react";
 
@@ -12,11 +12,37 @@ export default function Menu() {
     //https://res.cloudinary.com/dmtjposo5/gab/main/xiqdzedfefrvcmakfp6u
     //https://res.cloudinary.com/dmtjposo5/gab/dessert/jlusmortt9bltxktrzxw
     //https://res.cloudinary.com/dmtjposo5/gab/beverage/kcppriy7nikyshpzxjmr
+
+    // TEST https://soranews24.com/wp-content/uploads/sites/3/2021/10/pad-thai-oatmeal1.jpg?resize=640,480
     
     return (
         <>
-            <div className="center-align">
-                <br/><br/>
+            <Container>
+                <Row>
+                    <Col l={3}/>
+                    <Col l={6} className="center-align">
+                        <br/><br/>
+                        <h2>Menu Page</h2>
+                        <br/><br/>
+                        <Card
+                            actions={[
+                                <a key="1" href="#">This is a Link</a>
+                            ]}
+                            closeIcon={<Icon>close</Icon>}
+                            header={<CardTitle image="https://materializecss.com/images/sample-1.jpg">Card Title</CardTitle>}
+                            revealIcon={<Icon>more_vert</Icon>}
+                            >
+                            Here is the standard card with an image thumbnail.
+                        </Card>
+                    </Col>
+                    <Col l={3}/>
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+{/* <br/><br/>
                 <h2>Menu Page</h2>
                 <br/><br/>
                 <h4>Click to show polled API response:</h4>
@@ -30,8 +56,4 @@ export default function Menu() {
                 >Show API Data</Button>
                 <br/><br/>
                 {show && <h5>{data?.name}</h5>}
-                <br/><br/>
-            </div>
-        </>
-    )
-}
+                <br/><br/> */}
