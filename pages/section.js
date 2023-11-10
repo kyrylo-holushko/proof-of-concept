14a5bd2164f2dd4827755a8b37076b3ec4c79838
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-11-10 16:50:46 -0500

    cards work for appetizers

--- pages/section.js
+++ pages/section.js
@@ -1,6 +1,6 @@
 //import Card component
 import useSWR from "swr"
-import { Card, CardTitle, Icon, Col, Row } from "react-materialize";
+import { Card, CardTitle, Icon, Col, Row, Container } from "react-materialize";
 import { useContext } from "react";
 import { MenuContext } from "./_app";
 
@@ -14,29 +14,39 @@ export default function Section(){
 
     return (
         <>
-        <Col>
-            {data ? 
-            data.map(item=>{
-                return (
-                    <> 
-                        <Row className="p5">
-                            <Card
-                                actions={[
-                                    <a key="1" href="#">This is a link</a>
-                                ]}
-                                closeIcon={<Icon>close</Icon>}
-                                header={<CardTitle image={data?.image} />}
-                                horizontal
-                                revealIcon={<Icon>more_vert</Icon>}
-                                >
-                                {data?.description}
-                            </Card>
-                        </Row>
-                    </>
-                )
-            })  
-            : <h4>Error</h4>}
-            </Col>
+            <Container>
+                <Row>
+                    <Col l={3} m={2} s={1}/>
+                    <Col l={6} m={8} s={10} className="center-align">
+                        <br/><br/>
+                        <h3>Appetizers</h3>
+                        <br/><br/>
+                    {data ? 
+                    data.data.map(item=>{
+                        return (
+                            <> 
+                                <Row className="p1">
+                                    <Card
+                                        actions={[
+                                            <a key="1" href="#">This is a link</a>
+                                        ]}
+                                        closeIcon={<Icon>close</Icon>}
+                                        header={<CardTitle image={item?.image} />}
+                                        horizontal
+                                        revealIcon={<Icon>more_vert</Icon>}
+                                        className="menu-item"
+                                        >
+                                        {item?.description}
+                                    </Card>
+                                </Row>
+                            </>
+                        )
+                    })  
+                    : <h4>Error</h4>}
+                    </Col>
+                    <Col l={3} m={2} s={1}/>
+                </Row>
+            </Container>
         </>
     )
 
