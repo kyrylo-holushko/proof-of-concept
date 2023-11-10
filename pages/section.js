4faee5d519203adb5d7c9652d5b03d55061127e3
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2023-11-10 16:37:09 -0500

    added card mapping

--- pages/section.js
+++ pages/section.js
@@ -1,16 +1,42 @@
 //import Card component
 import useSWR from "swr"
-import { Card } from "react-materialize";
+import { Card, CardTitle, Icon, Col, Row } from "react-materialize";
+import { useContext } from "react";
+import { MenuContext } from "./_app";
 
 
 export default function Section(){
 
+    const { menu } = useContext(MenuContext);
+ 
     //const { data } = useSWR('http://localhost:3000/api/hello');
     const { data } = useSWR(menu ? `http://localhost:3000/api/${menu}` : null);
 
     return (
         <>
-
+        <Col>
+            {data ? 
+            data.map(item=>{
+                return (
+                    <> 
+                        <Row className="p5">
+                            <Card
+                                actions={[
+                                    <a key="1" href="#">This is a link</a>
+                                ]}
+                                closeIcon={<Icon>close</Icon>}
+                                header={<CardTitle image={data?.image} />}
+                                horizontal
+                                revealIcon={<Icon>more_vert</Icon>}
+                                >
+                                {data?.description}
+                            </Card>
+                        </Row>
+                    </>
+                )
+            })  
+            : <h4>Error</h4>}
+            </Col>
         </>
     )
 
