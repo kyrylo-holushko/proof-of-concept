2bb1a94dbf686bc95fea7c6a3433ba8a3df698ff
kyrylo-holushko <kyrylo_holushko@hotmail.com>
2024-01-07 13:04:28 -0500

    installed webpack

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
         <>
             <Head>              
                 <meta name="Golden Abalone Bistro" content="demo next app" />
+                <meta name="theme-color" content="#fbc02d"/>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
                 <link rel="stylesheet" href="path/to/materialize.css"></link>
                 <link rel="icon" href="/favicon.ico" />
